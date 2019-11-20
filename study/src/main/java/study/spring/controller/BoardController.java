865ce91b8b6455075ba09933e3e29ab92d79d2e7
YouHojoon <dbghwns66@daum.net>
2019-11-20 19:32:15 +0900

    fix:list controller and apply bootstrap theme

--- study/src/main/java/study/spring/controller/BoardController.java
+++ study/src/main/java/study/spring/controller/BoardController.java
@@ -1,16 +1,23 @@
 package study.spring.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -29,19 +36,27 @@ public class BoardController {
 	private BoardService service;
 	@Autowired
 	private ReplyService rservice;
+	private static final Logger log=LoggerFactory.getLogger(BoardController.class);
 	@GetMapping("/test")
 	public void test() {}
 	@GetMapping("/list")
-	public void list(@RequestParam("page") int page,Model model) {
-		model.addAttribute("serchList",service.selectBoardListSerch(new BoardPageDTO(page)));
-		model.addAttribute("pageMaker",new PageDTO(page, service.totoalCount(new BoardPageDTO(page))));
-	}
-	@PostMapping("/list")
-	public void postList(@RequestParam("page") int page,
-			@RequestParam("type") String type, @RequestParam("keyword") String keyword,Model model) {
-		BoardPageDTO boardPageDTO=new BoardPageDTO(page,type,keyword);
+	public void list(@RequestParam("page") int page,Model model,HttpSession session) {
+		BoardPageDTO boardPageDTO=null;
+		if(session.getAttribute("keyword")!=null&&session.getAttribute("type")!=null) {
+			log.info((String)session.getAttribute("keyword"));
+			boardPageDTO=new BoardPageDTO(page,(String)session.getAttribute("type"),(String)session.getAttribute("keyword"));
+		}
+		else
+			boardPageDTO=new BoardPageDTO(page);
+		
 		model.addAttribute("serchList",service.selectBoardListSerch(boardPageDTO));	
 		model.addAttribute("pageMaker",new PageDTO(page, service.totoalCount(boardPageDTO)));
+	}
+	@PostMapping("/list")
+	public void postList(@RequestParam("type") String type, @RequestParam("keyword") String keyword,Model model) {
+		BoardPageDTO boardPageDTO=new BoardPageDTO(1,type,keyword);
+		model.addAttribute("serchList",service.selectBoardListSerch(boardPageDTO));	
+		model.addAttribute("pageMaker",new PageDTO(1, service.totoalCount(boardPageDTO)));
 		model.addAttribute("type",type);
 		model.addAttribute("keyword",keyword);
 	}
